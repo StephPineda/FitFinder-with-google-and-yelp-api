f5eb48f1ac92167e198cf01f1de2dcb012fbaf54
StephPineda <stefoswimo@hotmail.com>
2019-05-31 14:52:56 -0400

    calender

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get  '/signup',  to: 'users#new'
 end
+resource :calender, only: [:show]
+end
