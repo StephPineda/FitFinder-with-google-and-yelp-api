e6618d61c25f95a511e53bcc454262fb00a8146b
Alena Bierzynski <abier070@uottawa.ca>
2019-06-01 17:13:06 -0400

    session login page functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  get 'sessions/new'
   root 'welcome#landing_page'
   resources :gyms
   resource :users
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
