d93ec4e8b321e8ebfa2a0aacbe0d1d4be9307635
Alena Bierzynski <abier070@uottawa.ca>
2019-06-02 13:15:04 -0400

    session log in broken

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    p user
+    if user && user.authenticate(params[:session][:password_digest])
+      log_in user
+      redirect_to user
+    else
+      alert 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
