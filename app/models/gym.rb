7648788fcd7fa5586a4fd3b9fed2de5eb80fa99e
Israel Martinez <marcemc@yahoo.com>
2019-06-04 13:58:53 -0400

    Styling fixes

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,4 +1,7 @@
 class Gym < ApplicationRecord
     has_many :events, dependent: :delete_all
     include SearchableByName
+
+    scope :by_zipcode, -> ( zipcode ){ where('address iLIKE ?', "%#{zipcode}%") }
+    scope :by_name, -> ( term ) { where("name ilike ?", "%#{term}%") }
 end
