3f6473163c04a87b0b02c600866f6873a1f37678
Alena Bierzynski <abier070@uottawa.ca>
2019-06-06 17:13:40 -0400

    calendar renders with gym info

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,9 +1,9 @@
 class Gym < ApplicationRecord
-    has_many :events, dependent: :delete_all
-    include SearchableByName
+  has_many :events, dependent: :delete_all
+  include SearchableByName
 
-    scope :by_zipcode, -> ( zipcode ){ where('address iLIKE ?', "%#{zipcode}%") }
-    scope :by_name, -> ( term ) { where("name ilike ?", "%#{term}%") }
+  scope :by_zipcode, -> ( zipcode ){ where('address iLIKE ?', "%#{zipcode}%") }
+  scope :by_name, -> ( term ) { where("name ilike ?", "%#{term}%") }
 
-     IMGS = ["yoga.jpg","yoga-1.jpg", "yoga-2.jpg","yoga-3.jpg", "yoga-4.jpg","yoga-5.jpg","yoga-6.jpg"]
+   IMGS = ["yoga.jpg","yoga-1.jpg", "yoga-2.jpg","yoga-3.jpg", "yoga-4.jpg","yoga-5.jpg","yoga-6.jpg"]
 end
